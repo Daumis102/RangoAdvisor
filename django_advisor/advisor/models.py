2e3ec3293ffc508219e94431b706c5e02aca2333
Daumantas Pagojus <2267352p@student.gla.ac.uk>
2018-02-23 10:15:14 +0000

    User model partly created

--- django_advisor/advisor/models.py
+++ django_advisor/advisor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# TODO PageModel, ReviewModel
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    # additional attributes
+    
+    picture = models.ImageField(upload_to='profile_image', blank=True)
+    # TODO add Reviews
+    # TODO add Pages visited
+
+    def __str__(self):
+        return self.user.username
