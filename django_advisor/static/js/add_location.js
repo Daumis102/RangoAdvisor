d979aab123286cec55ae43cf7ac67330f63fe71d
Anguel <angel.hristozov@gmail.com>
2018-03-02 19:41:45 +0000

    added basic google maps

--- django_advisor/static/js/add_location.js
+++ django_advisor/static/js/add_location.js
@@ -1,98 +1,14 @@
-$(document).ready(function(){
-    $('input[name=currentUrl]').val(window.location.pathname); // so we can redirect back to the original page after logging in
-    // ajax csrf setup
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-         function getCookie(name) {
-             var cookieValue = null;
-             if (document.cookie && document.cookie != '') {
-                 var cookies = document.cookie.split(';');
-                 for (var i = 0; i < cookies.length; i++) {
-                     var cookie = jQuery.trim(cookies[i]);
-                     // Does this cookie string begin with the name we want?
-                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                         break;
-                     }
-                 }
-             }
-             return cookieValue;
-         }
-         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-             // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-         }
-     }
-    });
-
-    // handle the user signup
-    $('#signupForm').submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: "/advisor/register/",
-            type: "POST",
-			dataType: "json",
-            data: $(this).serialize(),
-            success: function(resp) {
-                if (resp.statusCode === 0){
-                    // everything was ok, user is registered, redirect them to original page
-                    window.location = resp.currentUrl;
-                } else {
-                    alert('login failed');
-                    // TODO: alert the user here that their register failed
-                }
-            },
-            error: function(resp) {
-                console.log("error" + resp);
-            }
+function initMap(){
+        var uluru = {
+            lat: -25.363,
+            lng: 131.044
+        };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
         });
-    });
-
-    // handle the user login
-    $('#loginForm').submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: "/advisor/login/",
-            type: "POST",
-            dataType: "json",
-            data:$(this).serialize(),
-            success: function(resp) {
-                if (resp.statusCode === 0){
-                    // everything was ok, user is logged in, redirect them to original page
-                    window.location = resp.currentUrl;
-                } else {
-                    alert('login failed');
-                    // TODO: alert the user here that their login failed
-                }
-            },
-            error: function(resp) {
-                console.log(resp);
-            }
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
         });
-    })
-
-
-	// handle review
-    $('#reviewForm').submit(function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: "/advisor/write_review/",
-            type: "POST",
-            dataType: "json",
-            data:$(this).serialize(),
-            success: function(resp) {
-                if (resp.statusCode === 0){
-                    // everything was ok, review posted
-                    window.location = resp.currentUrl;
-                } else {
-                    alert('submit failed');
-                    // TODO: alert the user here that something went wrong
-                }
-            },
-            error: function(resp) {
-                console.log(resp);
-            }
-        });
-    })
-
-});
+}
