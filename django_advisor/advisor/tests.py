4fab5ca2f407b882c622435299985da161ad6c14
“Louis <2268980C@student.gla.ac.uk>
2018-03-19 16:04:54 +0000

    Working Tests for Models and Views (46% Code Coverage)

--- django_advisor/advisor/tests.py
+++ django_advisor/advisor/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
 
-# Create your tests here.
+class ModelsTest(TestCase):
+    def setUp(self):
+        Location.objects.create(name="Caledonian UNi", city="Glasgow", coordinates="233, 233", slug="Caley")
+
+    def test_get_lat(self):
+        loc = Location.objects.get(city="Glasgow")
+        self.assertEquals(loc.get_lat(), 233.0)
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        Location.objects.create(name="Caledonian UNi", city="Glasgow", coordinates="233, 233", slug="Caley")
+
+    def test_index_page(self):
+        c = Client()
+        response = c.get("/advisor/index/")
+        self.assertContains(response, "Caledonian")
