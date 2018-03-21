110d3d3751e42c61847a6a91bfc617ee53bbbc79
Daumantas Pagojus <2267352p@student.gla.ac.uk>
2018-03-21 00:40:48 +0000

    added localhost to alowed hosts

--- django_advisor/django_advisor/settings.py
+++ django_advisor/django_advisor/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = 'of6j3q36=jbk*u=)xsh5e9v)u*38vcbge4p_=gqi$jhyzil+9y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoadvisor.pythonanywhere.com']
+ALLOWED_HOSTS = ['djangoadvisor.pythonanywhere.com',
+				'localhost',
+				'127.0.0.1']
 
 
 # Application definition
