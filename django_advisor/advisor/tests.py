c0159d97485879f53e3a020cde2f4317ded1599a
“Louis <2268980C@student.gla.ac.uk>
2018-03-26 19:44:34 +0100

    test of contacts and about page test of the user signup form

--- django_advisor/advisor/tests.py
+++ django_advisor/advisor/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from .models import *
 
+
 class ModelsTest(TestCase):
     def setUp(self):
         Location.objects.create(name="Caledonian UNi", city="Glasgow", coordinates="233, 233", slug="Caley")
@@ -9,11 +10,34 @@ class ModelsTest(TestCase):
         loc = Location.objects.get(city="Glasgow")
         self.assertEquals(loc.get_lat(), 233.0)
 
+
+
+
 class ViewsTest(TestCase):
     def setUp(self):
-        Location.objects.create(name="Caledonian UNi", city="Glasgow", coordinates="233, 233", slug="Caley")
+        Location.objects.create(name="Caledonian Uni", city="Glasgow", coordinates="233, 233", slug="Caley")
 
     def test_index_page(self):
         c = Client()
         response = c.get("/advisor/index/")
         self.assertContains(response, "Caledonian")
+
+    def test_contacts_page(self):
+        c = Client()
+        response = c.get("/advisor/contacts/")
+        self.assertContains(response, "Anguel")
+
+    def test_about_page(self):
+        c = Client()
+        response = c.get("/advisor/contacts/")
+        self.assertContains(response, "DjangoAdvisor")
+
+
+class FormsTest(TestCase):
+
+    def test_signup_form(self):
+        c = Client()
+        c.post("forms/signupForm/", {"username": ["test"], "password": ["test"], "passwordConfirm": ["test"]})
+        self.assertTrue(UserProfile.objects.filter(username="test"))
+
+
